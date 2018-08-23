894de309b6651dccf375b9c46e1518c671f79ecc
samao <qiyanlong@wozine.com>
2018-08-23 12:16:15 +0800

    1

--- js/home.js
+++ js/home.js
@@ -59,7 +59,7 @@
             //这里this绑定为player实例
         },
         type: 'h5',
-        kernel: 'hls',
+        kernel: 'ks',
         backgroundURL: 'http://imgs.aixifan.com/content/2018_07_12/1531390013.jpg',
     });
 
