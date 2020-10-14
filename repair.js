c4b2c80d28024c16c3f7d02d923e916352da40a8
qiyanlong <qiyanlong@qie.tv>
2020-10-14 11:13:46 +0800

    feat: 弹幕生成工具

---
+++ repair.js
@@ -0,0 +1,32 @@
+const mockJS = require("mockjs");
+const fs = require("fs");
+const path = require("path");
+
+let [, , size = 1000, duration = 10 * 60] = process.argv;
+
+size = Number.isNaN(parseInt(size)) ? 1000 : parseInt(size);
+const sizeStream = fs.createWriteStream(path.join(__dirname, "size", "9527"));
+sizeStream.write(JSON.stringify([0, 0, size]));
+
+
+const danmu = Array.from({length: size}, () => {
+    const { Random } = mockJS;
+    const time = Random.integer(0, duration);
+    const color = Random.integer(0, 0xFFFFFF);
+    
+    const MODE_RAN = Math.random();
+    const mode = MODE_RAN > 0.8 ? 1 : MODE_RAN > 0.9 ? 5 : 4;
+
+    const SIZE_RAN = Math.random();
+    const fontSize = SIZE_RAN > 0.8 ? 25 : 37;
+
+    const uuid = Random.uuid();
+
+    return {
+        c: `${time},${color},${mode},${fontSize},13143927,1523267343,${uuid}`,
+        m: Random.csentence(3, 15)
+    }
+})
+
+const danmuStream = fs.createWriteStream(path.join(__dirname, 'V4', '9527_2', '8888', '500'));
+danmuStream.write(JSON.stringify([[],[], danmu]));
