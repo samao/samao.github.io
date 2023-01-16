7f29047154b19133337789a6d21b234f02c97a6b
qiyanlong <qiyanlong@qie.tv>
2023-01-16 18:08:45 +0800

    test: live demo

--- js/q-player-plus.js
+++ js/q-player-plus.js
@@ -1,5 +1,6 @@
 (function () {
-    const config = { isLive: false, demuxType: "m3u8", rid: 10001822, streamHost: "" };
+    const isLive = location.search.includes('live')
+    const config = { isLive, demuxType: isLive ? "flv": 'm3u8', rid: 10001822 }; //{ isLive: false, demuxType: "m3u8", rid: 10001822, streamHost: "" };
     QiePlayer.createPlayer({
         ...config,
         vid: config.rid,
@@ -42,19 +43,37 @@
         //flashUrl: '/qie-flashplayer.swf',
         // plugins: ["banner"],
         loader: {
+            rtmp() {
+                return {
+                    error: 0,
+                    data: {
+                        rtmp_url: "https://las-tech.org.cn/kwai",
+                        rtmp_live: "las-test_ld500d.flv",
+                        rtmp_cdn: "ws",
+                        cdns: ["ws"]
+                    },
+                };
+            },
+            room() {
+                return {
+                    giftBatterConfig: [],
+                    giftExBatterConfig: [],
+                    show_status: "1",
+                };
+            },
             vod(video_id) {
                 // console.log("拉取VOD", video_id);
                 return Promise.resolve({
                     error: 0,
                     video_info: {
-                        "stream_name": "hackerfucked",
-                        "user_info": {
-                          "nickname": "黑客是撒币",
-                          "user_pic": "https://tx-free-imgs2.acfun.cn/kimg/bs2/zt-image-host/ChYwOGFhYTc4YzRmMTBjNmU1OTVhZTA2EJvM1y8.webp",
-                          "room_id": "#",
-                          "follow_state": 1
-                        }
-                      },
+                        stream_name: "hackerfucked",
+                        user_info: {
+                            nickname: "黑客是撒币",
+                            user_pic: "https://tx-free-imgs2.acfun.cn/kimg/bs2/zt-image-host/ChYwOGFhYTc4YzRmMTBjNmU1OTVhZTA2EJvM1y8.webp",
+                            room_id: "#",
+                            follow_state: 1,
+                        },
+                    },
                 });
             },
         },
